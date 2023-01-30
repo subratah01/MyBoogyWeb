4d9f18a72415f42575fc3006664df7cba30a224b
juin kar <juinkar@gmail.com>
2023-01-30 22:36:50 +0530

    Comment Added

--- OA.Model/UserLogin.cs
+++ OA.Model/UserLogin.cs
@@ -22,46 +22,46 @@ namespace OA.Model
 
 
 
-    public class UserDataRequest
-    {        
-        public string MobileNo { get; set; }
-        public string OTP { get; set; }
-        public string UserName { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string LocationId { get; set; }
-        public string Pincode { get; set; }
+    //public class UserDataRequest
+    //{        
+    //    public string MobileNo { get; set; }
+    //    public string OTP { get; set; }
+    //    public string UserName { get; set; }
+    //    public string FirstName { get; set; }
+    //    public string LastName { get; set; }
+    //    public string Email { get; set; }
+    //    public string LocationId { get; set; }
+    //    public string Pincode { get; set; }
 
-    }
-    public class AdminLoginRequest
-    {
-        public string Email { get; set; }
-        public string Pwd { get; set; }
+    //}
+    //public class AdminLoginRequest
+    //{
+    //    public string Email { get; set; }
+    //    public string Pwd { get; set; }
 
-    }
+    //}
 
-    public class OTPData
-    {
-        public string MobileNo { get; set; }
-        public string OTP { get; set; }
-    }
+    //public class OTPData
+    //{
+    //    public string MobileNo { get; set; }
+    //    public string OTP { get; set; }
+    //}
 
-    public class UserLoginInformation
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string DisplayName { get; set; }
-        public int? LocationId { get; set; }
-        public string Token { get; set; }
-        public long UserId { get; set; }
-        public string Mobile { get; set; }
-        public string Email { get; set; }
-        public long RoleId { get; set; }
-        public string RoleName { get; set; }
+    //public class UserLoginInformation
+    //{
+    //    public string FirstName { get; set; }
+    //    public string LastName { get; set; }
+    //    public string DisplayName { get; set; }
+    //    public int? LocationId { get; set; }
+    //    public string Token { get; set; }
+    //    public long UserId { get; set; }
+    //    public string Mobile { get; set; }
+    //    public string Email { get; set; }
+    //    public long RoleId { get; set; }
+    //    public string RoleName { get; set; }
 
-        //public string LocationName { get; set; }
-        //public string Pincode { get; set; }
+    //    //public string LocationName { get; set; }
+    //    //public string Pincode { get; set; }
 
-    }
+    //}
 }
